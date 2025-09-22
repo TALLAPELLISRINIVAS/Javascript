606dc46b76e85f1bdd4e4dd262e12474ededcb0b
srinivas <srinivastallapelli8@gmail.com>
2025-09-22 12:38:10 +0530

    changes

---
+++ function.js
@@ -0,0 +1,33 @@
+//function
+//code repository
+// function add(){
+//     var a = 67;
+//     var b = 78;
+//     document.write(a + b);
+//     console.log(a + b);
+// }
+
+// add(); 
+// const add = () => {
+//     var a = 67;
+//     var b = 78;
+//     document.write(a + b);
+//     console.log(a + b);
+// };
+// add();
+// const add = (a,b) => console.log(a + b);
+// // add(78,90);
+// console.log(b);
+// b = 45;
+
+// console.log(b);
+// b = 45;
+
+let name = "srinu";
+
+function demo() {
+    let name = "srinu";
+    document.write("Hello, my name is $(name)");
+}
+document.write("Hello, my name is $(name)");
+demo();
