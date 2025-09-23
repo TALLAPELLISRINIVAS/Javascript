1f250dc057461d194627316f51e07e6d89fe6c8b
srinivas <srinivastallapelli8@gmail.com>
2025-09-23 12:21:41 +0530

    changes

---
+++ predefined.js
@@ -0,0 +1,7 @@
+var a = Number(prompt("Enter value a"));
+var b = Number(prompt("Enter value b"));
+
+document.write("entered value a and b is $(),$()");
+function add() {
+    console.log(a + b);
+}
